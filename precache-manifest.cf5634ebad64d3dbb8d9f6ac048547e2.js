a4a7220a867a00747cd8dd02fae426e4a6a918cf
karental <krntal3@gmail.com>
2019-11-16 20:11:10 +0200

    Updates

---
+++ precache-manifest.cf5634ebad64d3dbb8d9f6ac048547e2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60a74bc16ed2bd846872dc870f187104",
+    "url": "/Karen-Tal-09-11-19/index.html"
+  },
+  {
+    "revision": "f74f96acfd68bd376fe8",
+    "url": "/Karen-Tal-09-11-19/static/css/main.8e14038a.chunk.css"
+  },
+  {
+    "revision": "10cf43702e15560d4c1b",
+    "url": "/Karen-Tal-09-11-19/static/js/2.aa25d898.chunk.js"
+  },
+  {
+    "revision": "f74f96acfd68bd376fe8",
+    "url": "/Karen-Tal-09-11-19/static/js/main.14d8790e.chunk.js"
+  },
+  {
+    "revision": "ac6505c996cb8a7349b8",
+    "url": "/Karen-Tal-09-11-19/static/js/runtime-main.9ee1a580.js"
+  },
+  {
+    "revision": "8f803e2a86727a771e96481dc1a25195",
+    "url": "/Karen-Tal-09-11-19/static/media/back.8f803e2a.jpg"
+  },
+  {
+    "revision": "c819dfae5355d599554d76eb0248fda6",
+    "url": "/Karen-Tal-09-11-19/static/media/day.c819dfae.jpg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/Karen-Tal-09-11-19/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/Karen-Tal-09-11-19/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/Karen-Tal-09-11-19/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/Karen-Tal-09-11-19/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
